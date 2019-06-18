e1299c0bb392d1adc3beeebba06840c29fb899d7
irfanp90 <irfanp90@gmail.com>
2019-06-18 12:43:02 -0500

    update js,style

--- JSClock/clock.js
+++ JSClock/clock.js
@@ -1,4 +1,6 @@
 const secondHand = document.querySelector(".second-hand");
+const minHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
 function setDate(){
     // console.log("one")
     const now = new Date()
@@ -6,9 +8,17 @@ function setDate(){
     const seconds = now.getSeconds();
     const secondDegrees = ((seconds / 60) *360) + 90;
     secondHand.style.transform = `rotate(${secondDegrees}deg)`
-    console.log(seconds);
+    console.log("sec"+ seconds);
 
+    const min = now.getMinutes()
+    const minDegrees = ((min / 60)* 360) + 90;
+    minHand.style.transform = `rotate(${minDegrees}deg)`
+    console.log("min" + min)
 
+    const hour = now.getHours()
+    const hourDegrees = ((hour / 12)* 360) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`
+    console.log("hour" + hour)
 }
 
 setInterval(setDate, 1000)
